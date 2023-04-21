888c12306c04a83649d7473c9746f992730d9bfc
hashirshoaeb <hashirshoaeb@gmail.com>
2023-04-21 16:42:23 +0500

    initial website

--- src/app/page.js
+++ src/app/page.js
@@ -1,102 +1,78 @@
+'use client';
 import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react';
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import styles from './custom.global.scss'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <Nav />
+      <div className="min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0">
+        <div className="container">
+          <div className=" row align-items-center">
+            <div className="col-sm-6">
+              <h1 className="text-primary fw-bold display-3">We Build <span className='text-secondary'>Mobile Apps </span> That Engage & Convert Users.</h1>
+              <p>{"Comprehensive mobile app development & marketing services that deliver guaranteed results. Let's get started!"}</p>
+              <div className="text-center">
+                {/* <a className="btn btn-primary my-1 mx-3 px-5 py-2">Website Comming Soon</a> */}
+                <a className="btn btn-secondary my-1 mx-3 px-5 py-2">Contact us</a>
+              </div>
+            </div>
+            {/* <div className="col-sm-6 text-center"> */}
+            {/* <h1 className="text-primary fw-bold">Blocship</h1> */}
+            {/* <p>Comming Soon</p> */}
+            {/* <div className="text-center">
+                <a className="btn btn-primary my-1 mx-3">Comming Soon</a>
+              </div> */}
+            {/* </div> */}
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
 }
+
+
+const Nav = () => {
+
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  return (
+    <nav className="navbar navbar-expand-sm navbar-light">
+      <div className="container">
+        {/* <Link href="/"> */}
+        {/* <Image src={Logo} alt="Logo" width="36" height="36" className="vertical-align-middle" /> */}
+        <a className="navbar-brand">
+          <span className="">Blocship</span>
+        </a>
+        {/* </Link> */}
+        <button
+          className="custom-toggler navbar-toggler"
+          type="button" data-toggle="collapse"
+          data-target="#navbarsExample09"
+          aria-controls="navbarsExample09"
+          aria-expanded={!isNavCollapsed ? true : false}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`}
+          id="navbarsExample09"
+        >
+          <div className="navbar-nav">
+            <a className="nav-link">Contact us</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
