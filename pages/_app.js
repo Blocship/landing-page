38dadb6f9e7ef9f2220e982ff37d026084f2586c
saad <saadgfx97@gmail.com>
2023-06-26 12:40:35 +0500

    added inter font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import { Inter } from 'next/font/google'
 import '../styles/globals.css'
 
+const inter = Inter({ subsets: ['latin'] })
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
