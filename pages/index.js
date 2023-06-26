bd275b5eedfc30c251333fbbc9311b998a1dbc73
saad <saadgfx97@gmail.com>
2023-06-26 12:18:05 +0500

    init

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Blocship</title>
+        <meta name="description" content="We Create Flutter Centric Mobile Apps That Engage & Convert Users." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>We Create Flutter Centric Mobile Apps That Engage & Convert Users.</h1>
+      </main>
+    </div>
+  )
+}
