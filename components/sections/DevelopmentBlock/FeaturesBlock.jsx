4c27a83ce6bb8957d607064981b086137bb5f738
saad <saadgfx97@gmail.com>
2023-07-11 15:29:36 +0500

    features block

--- components/sections/DevelopmentBlock/FeaturesBlock.jsx
+++ components/sections/DevelopmentBlock/FeaturesBlock.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import LetsDiscuss from './LetsDiscuss'
 
 export default function FeaturesBlock() {
   return (
@@ -18,9 +19,7 @@ export default function FeaturesBlock() {
         </div>
       </div>
       
-      <button className='px-[30px] py-[15px] md:px-[24px] md:py-[16px] lg:px-[30px] lg:py-[15px] rounded-full bg-[#F9A920] text-[#1F1F1F] font-normal text-[16px] self-start'>
-        Let discuss your app idea!
-      </button>
+      <LetsDiscuss className={'hidden md:block'}/>
     </div>
   )
 }
---
+++ components/sections/DevelopmentBlock/LetsDiscuss.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LetsDiscuss({className}) {
+  return (
+    <button className={`${className} px-[30px] py-[15px] md:px-[24px] md:py-[16px] lg:px-[30px] lg:py-[15px] rounded-full bg-[#F9A920] text-[#1F1F1F] font-normal text-[16px] self-start`}>
+      Let discuss your app idea!
+    </button>
+  )
+}
